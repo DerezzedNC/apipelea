bc106a7a0a18b624199f2d2e9f33109bbc5ad68c
Angel Noh <angelgabrielnohcamargo@gmail.com>
2025-07-30 15:46:06 -0600

    actualizacion 41

--- public/js/seleccion.js
+++
@@ -1,8 +0,0 @@
-function elegirModo(modo) {
-    if (modo === '1vs1') {
-      window.location.href = 'batalla1vs1.html'; // Página de batalla individual
-    } else if (modo === '3vs3') {
-      window.location.href = 'seleccion.html'; // Página de selección de personajes 3vs3
-    }
-  }
-  
